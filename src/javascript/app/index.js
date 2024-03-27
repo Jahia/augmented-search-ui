31d1014d7bb83fa4bc14719bc7a1d66d04df32c1
hduchesne <hduchesne@jahia.com>
2024-03-27 18:38:01 +0100

    review content result layout fix multilingual support for date in result...

--- src/javascript/app/index.js
+++ src/javascript/app/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import i18n from './i18n/i18n';
+import moment from 'moment';
+import 'moment/locale/fr';
 
 const bootstrap = function (target, context) {
     i18n.changeLanguage(context.language);
+    moment().locale(context.language);
     const root = ReactDOM.createRoot(document.getElementById(target));
     root.render(
         <App dxContext={context}/>
