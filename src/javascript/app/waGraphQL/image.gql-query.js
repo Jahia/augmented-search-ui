1e0c268b7f6e7ccba9eca72e968243efcd04dd9d
hduchesne <hduchesne@jahia.com>
2024-06-24 18:14:35 +0200

    add apollo, write the query... no need to see if I can catch props image from index

--- src/javascript/app/waGraphQL/image.gql-query.js
+++ src/javascript/app/waGraphQL/image.gql-query.js
@@ -1,17 +1,19 @@
-import {gql} from "@apollo/client";
-import {CORE_NODE_FIELDS} from "./fragments"
+import {gql} from '@apollo/client';
+import {CORE_NODE_FIELDS} from './fragments';
 
 export const GetImage = gql`
     ${CORE_NODE_FIELDS}
     query getImage($workspace: Workspace!, $id: String!, $language: String!) {
         response: jcr(workspace: $workspace) {
             workspace
-            media: nodeById(uuid: $id) {
+            content: nodeById(uuid: $id) {
                 ...CoreNodeFields
-                title:displayName(language:$language)
-                ajaxRenderUrl
+                image : property(name:"image",){ node: refNode {
+                    ...CoreNodeFields
+                    title:displayName(language:$language)
+                    ajaxRenderUrl
+                } }
             }
         }
-    }`
-
+    }`;
 
